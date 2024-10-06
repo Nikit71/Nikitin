a358d487482b9edfa690ea5dadd0c9afe6d6f649
Nikit71 <nikitural@gmail.com>
2024-10-06 11:50:28 +0300

    Add files via upload

---
+++ Lesson0-2.py
@@ -0,0 +1,24 @@
+# Вводный модуль
+# Практическое задание по уроку "Базовые структуры данных"
+
+# 1st program
+print(9**0.5*5)
+
+# 2nd program
+print(99.99>99.98 and 1000!=1000.1)
+
+# 3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+# 4th program
+x='123.456'
+print(type(x))
+x=float(x)
+print(type(x))
+x=int(x*10)
+print(x)
+x=x%10
+print('Результат-',x)
+print('Результат одной строкой-',int(float('123.456')*10)%10)
